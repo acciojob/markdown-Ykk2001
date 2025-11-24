515b005dbe89e376871f6bb5531dcc460880a961
Ykk2001 <yogeshkhatake18@gmail.com>
2025-11-24 20:59:49 +0530

    Done1

--- src/components/MarkDownEditor.js
+++ src/components/MarkDownEditor.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import Markdown from 'react-markdown'
 export default function MarkDownEditor() {
-const[input,setInput]=useState('# Hello');
+const[input,setInput]=useState('# Heading');
 const[preview,setPreview]=useState('')
 
 useEffect(()=>{
