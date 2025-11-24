6be072f1d7c532743b34ac49b74db9bd1b630667
Ykk2001 <yogeshkhatake18@gmail.com>
2025-11-24 20:21:09 +0530

    Done

---
+++ src/components/MarkDownEditor.js
@@ -0,0 +1,24 @@
+import React, { useEffect, useState } from 'react'
+import Markdown from 'react-markdown'
+export default function MarkDownEditor() {
+const[input,setInput]=useState('# Hello');
+const[preview,setPreview]=useState('')
+
+useEffect(()=>{
+   setPreview(input)
+},[input])
+  return (
+    <div className='container'>
+        
+        <textarea className='textarea' placeholder='Write markdown text here...' value={input} onChange={(e)=>setInput(e.target.value)}>
+
+        </textarea>
+
+        <div className='preview'>
+         <Markdown >{preview}</Markdown>
+        </div>
+    </div>
+  )
+}
+
+// 
